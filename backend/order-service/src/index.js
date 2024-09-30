5d8c61b48a52f9c2a6b89f1232400b961b86e8a8
Mohammad Shabeer <moshdev2213@gmail.com>
2024-09-30 20:41:22 +0530

    fix:serverRes

--- backend/order-service/src/index.js
+++ backend/order-service/src/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors(corsConfig));
 
 app.get("/", async (req, res) => {
-  res.status(200).json({ 'message': 'server online' });
+  res.status(200).json({ 'message': 'Server Online' });
 })
 
 app.post('/publish', async (req, res) => {
